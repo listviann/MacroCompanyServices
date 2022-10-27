e06d1dae97625c0a5ec1bb7dd9660f3262e52a39
listviann <mixail.larionov.2002@mail.ru>
2022-10-28 00:18:14 +0500

    Added a new route for the user area.

--- MacroCompanyServices/Program.cs
+++ MacroCompanyServices/Program.cs
@@ -79,6 +79,10 @@ app.MapControllerRoute(
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
+    name: "user",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
