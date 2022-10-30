2220351ebe6d584deba05eeac0d5f0efc7c12159
listviann <mixail.larionov.2002@mail.ru>
2022-10-31 02:02:22 +0500

    Added logic for UserProfile.Index.

--- MacroCompanyServices/Areas/User/Controllers/UserProfileController.cs
+++ MacroCompanyServices/Areas/User/Controllers/UserProfileController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MacroCompanyServices.Areas.User.Controllers
 {
     [Area("User")]
     public class UserProfileController : Controller
     {
+        public IActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userName = User.FindFirstValue(ClaimTypes.Name);
 
+            ViewBag.userId = new Guid(userId);
+            ViewBag.userName = userName;
+            return View();
+        }
     }
 }
