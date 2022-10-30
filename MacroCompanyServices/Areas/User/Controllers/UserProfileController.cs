8a115db927b35e2a3adb46e3f2f8c868575a6490
listviann <mixail.larionov.2002@mail.ru>
2022-10-31 03:24:21 +0500

    Added profile editing.

--- MacroCompanyServices/Areas/User/Controllers/UserProfileController.cs
+++ MacroCompanyServices/Areas/User/Controllers/UserProfileController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MacroCompanyServices.Areas.User.Models;
+using MacroCompanyServices.Service;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace MacroCompanyServices.Areas.User.Controllers
@@ -6,6 +9,13 @@ namespace MacroCompanyServices.Areas.User.Controllers
     [Area("User")]
     public class UserProfileController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserProfileController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -14,6 +24,29 @@ namespace MacroCompanyServices.Areas.User.Controllers
             ViewBag.userId = new Guid(userId);
             ViewBag.userName = userName;
             return View();
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            var model = new EditUserViewModel() { Id = new Guid(user.Id) };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id.ToString());
+
+            if (ModelState.IsValid)
+            {
+                user.UserName = model.UserName;
+                var result = await _userManager.UpdateAsync(user);
+                return RedirectToAction(nameof(UserProfileController.Index), nameof(UserProfileController).CutController());
+            }
+            
+            return View(model);
         }
     }
 }
---
+++ MacroCompanyServices/Areas/User/Models/EditUserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MacroCompanyServices.Areas.User.Models
+{
+    public class EditUserViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string? UserName { get; set; }
+    }
+}
